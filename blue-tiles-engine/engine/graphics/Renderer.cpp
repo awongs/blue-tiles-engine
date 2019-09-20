e05262b41ec5f1128a58f0d70ff70fb5105671c5
Anthony Wong <awong6201@gmail.com>
2019-09-19 19:53:28 -0700

    Removed placeholder shader strings

--- blue-tiles-engine/engine/graphics/Renderer.cpp
+++ blue-tiles-engine/engine/graphics/Renderer.cpp
@@ -35,11 +35,13 @@ void Renderer::SetupShaders()
 	GLuint fragmentShader;
 	GLuint shaderProgram;
 
+	// Load shader files into strings
 	std::string vertexSource = filemanager::LoadFile("engine/graphics/shaders/VertexShader.glsl");
+	std::string fragmentSource = filemanager::LoadFile("engine/graphics/shaders/FragmentShader.glsl");
 
 	vertexShader = m_shaderManager->CompileShader(GL_VERTEX_SHADER, vertexSource.c_str());
 
-	fragmentShader = m_shaderManager->CompileShader(GL_FRAGMENT_SHADER, m_fragmentSource);
+	fragmentShader = m_shaderManager->CompileShader(GL_FRAGMENT_SHADER, fragmentSource.c_str());
 
 	shaderProgram = m_shaderManager->CreateShaderProgram(vertexShader, fragmentShader);
 
