a011e3cb91e506e03c4462468481fbfa84f0a8f5
Minitte <19943415+Minitte@users.noreply.github.com>
2019-09-30 21:02:33 -0700

    Replaced window code in main

--- blue-tiles-engine/Main.cpp
+++ blue-tiles-engine/Main.cpp
@@ -9,14 +9,16 @@
 #include "engine/input/Input.h"
 #include "engine/debugbt/DebugLog.h"
 #include "engine/sound/Sound.h"
+#include "engine/GameWindow.h"
 
 int main()
 {
-	// Initialize SDL.
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)
+	GameWindow gameWin(800, 600);
+
+	int windowSetupStatus = gameWin.SetupSDLWindow();
+
+	if (windowSetupStatus)
 	{
-		std::cout << "Could not initialize SDL: " << SDL_GetError() << 
-			std::endl;
 		return 1;
 	}
 
@@ -26,33 +28,8 @@ int main()
 		return 2;
 	}
 
-	SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-
-	// Create the window.
-	SDL_Window *window{ nullptr };
-	window = SDL_CreateWindow(
-		"Blue Tiles Engine",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		800,
-		600,
-		SDL_WINDOW_OPENGL);
-
-	// Check if the window was created.
-	if (window == nullptr)
-	{
-		std::cout << "Window could not be created: " << SDL_GetError() <<
-			std::endl;
-		return 1;
-	}
-
 	// create game engine
-	GameEngine* engine = new GameEngine(window);
+	GameEngine* engine = new GameEngine(gameWin.GetWindow());
 
 	// Create the input manager.
 	Input *input{ new Input() };
@@ -110,10 +87,6 @@ int main()
 	// destroy engine
 	delete engine;
 	engine = NULL;
-
-	// Clean up SDL.
-	SDL_DestroyWindow(window);
-	SDL_Quit();
 
 	return 0;
 }
