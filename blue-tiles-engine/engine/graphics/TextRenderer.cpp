73731bd97bca49b2593451b048018543bdb23576
Minitte <19943415+Minitte@users.noreply.github.com>
2019-10-07 20:09:16 -0700

    Created TextRenderer
    https://learnopengl.com/In-Practice/Text-Rendering

---
+++ blue-tiles-engine/engine/graphics/TextRenderer.cpp
@@ -0,0 +1,180 @@
+#include "TextRenderer.h"
+#include "ShaderManager.h"
+#include "Shader.h"
+#include "../../util/FileManager.h"
+#include "../debugbt/DebugLog.h"
+
+TextRenderer::TextRenderer(int windowWidth, int windowHeight)
+	: m_width(windowWidth)
+	, m_height(windowHeight)
+{
+	m_shaderManager = new ShaderManager();
+
+	SetupTextShader();
+
+	SetupFreeText();
+}
+
+TextRenderer::~TextRenderer()
+{
+}
+
+void TextRenderer::RenderText(std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color)
+{
+	// Activate corresponding render state	
+	m_shaderManager->UseShaderProgram(m_shader->GetProgramHandle());
+	m_shader->SetUniform3f("textColor", color);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindVertexArray(m_vao);
+
+	// Iterate through all characters
+	std::string::const_iterator c;
+	for (c = text.begin(); c != text.end(); c++)
+	{
+		Character ch = m_characters[*c];
+
+		GLfloat xpos = x + ch.Bearing.x * scale;
+		GLfloat ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+		GLfloat w = ch.Size.x * scale;
+		GLfloat h = ch.Size.y * scale;
+		// Update VBO for each character
+		GLfloat vertices[6][4] = {
+			{ xpos,     ypos + h,   0.0, 0.0 },
+			{ xpos,     ypos,       0.0, 1.0 },
+			{ xpos + w, ypos,       1.0, 1.0 },
+
+			{ xpos,     ypos + h,   0.0, 0.0 },
+			{ xpos + w, ypos,       1.0, 1.0 },
+			{ xpos + w, ypos + h,   1.0, 0.0 }
+		};
+		// Render glyph texture over quad
+		glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+
+		// Update content of VBO memory
+		glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // Be sure to use glBufferSubData and not glBufferData
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		
+		// Render quad
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		
+		// Now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+		x += (ch.Advance >> 6) * scale; // Bitshift by 6 to get value in pixels (2^6 = 64 (divide amount of 1/64th pixels by 64 to get amount of pixels))
+	}
+	glBindVertexArray(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+
+void TextRenderer::SetupTextShader()
+{
+	// id of shader programs
+	GLuint vertexShader;
+	GLuint fragmentShader;
+
+	// file
+	std::string vertex = filemanager::LoadFile("../Assets/shaders/ShaderText.vsh");
+	std::string fragment = filemanager::LoadFile("../Assets/shaders/ShaderText.fsh");
+
+	// shader compiling
+	vertexShader = m_shaderManager->CompileShader(GL_VERTEX_SHADER, vertex.c_str());
+	fragmentShader = m_shaderManager->CompileShader(GL_FRAGMENT_SHADER, fragment.c_str());
+
+	// shader program
+	m_shader = m_shaderManager->CreateShaderProgram(vertexShader, fragmentShader);
+
+	// set projeection for rendering
+	glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(m_width), 0.0f, static_cast<GLfloat>(m_height));
+	m_shader->SetUniformMatrix4fv("projection", projection);
+	
+}
+
+void TextRenderer::SetupFreeText()
+{
+	// Set OpenGL options
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	// FreeType
+	FT_Library ft;
+	// All functions return a value different than 0 whenever an error occurred
+	if (FT_Init_FreeType(&ft))
+	{
+		DebugLog::Info("ERROR::FREETYPE: Could not init FreeType Library");
+	}
+
+	// Load font as face
+	FT_Face face;
+	if (FT_New_Face(ft, "fonts/arial.ttf", 0, &face))
+	{
+		DebugLog::Info("ERROR::FREETYPE: Failed to load font");
+	}
+
+	// Set size to load glyphs as
+	FT_Set_Pixel_Sizes(face, 0, 48);
+
+	// Disable byte-alignment restriction
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	// Load first 128 characters of ASCII set
+	for (GLubyte c = 0; c < 128; c++)
+	{
+		// Load character glyph 
+		if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+		{
+			DebugLog::Info("ERROR::FREETYTPE: Failed to load Glyph");
+			continue;
+		}
+
+		// Generate texture
+		GLuint texture;
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glTexImage2D(
+			GL_TEXTURE_2D,
+			0,
+			GL_RED,
+			face->glyph->bitmap.width,
+			face->glyph->bitmap.rows,
+			0,
+			GL_RED,
+			GL_UNSIGNED_BYTE,
+			face->glyph->bitmap.buffer
+		);
+
+		// Set texture options
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		// Now store character for later use
+		Character character = {
+			texture,
+			glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+			glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+			face->glyph->advance.x
+		};
+		m_characters.insert(std::pair<GLchar, Character>(c, character));
+	}
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	// Destroy FreeType once we're finished
+	FT_Done_Face(face);
+	FT_Done_FreeType(ft);
+
+	// Configure VAO/VBO for texture quads
+	glGenVertexArrays(1, &m_vao);
+	glGenBuffers(1, &m_vbo);
+	glBindVertexArray(m_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
---
+++ blue-tiles-engine/engine/graphics/TextRenderer.h
@@ -0,0 +1,58 @@
+#pragma once
+
+// std includes
+#include <map>
+#include <string>
+#include <memory>
+
+// sdl
+#include <sdl2/SDL.h>
+
+// opengl
+#include <glad/glad.h>
+
+// GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// FreeType
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+class ShaderManager;
+class Shader;
+
+struct Character {
+	GLuint TextureID;   // ID handle of the glyph texture
+	glm::ivec2 Size;    // Size of glyph
+	glm::ivec2 Bearing;  // Offset from baseline to left/top of glyph
+	GLuint Advance;    // Horizontal offset to advance to next glyph
+};
+
+class TextRenderer
+{
+public:
+	TextRenderer(int windowWidth, int windowHeight);
+
+	~TextRenderer();
+
+	void RenderText(std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color);
+
+private:
+
+	void SetupTextShader();
+
+	void SetupFreeText();
+
+	std::shared_ptr<Shader> m_shader;
+
+	int m_width, m_height;
+
+	GLuint m_vao, m_vbo;
+
+	std::map<GLchar, Character> m_characters;
+
+	ShaderManager* m_shaderManager;
+
+};
