219da6ffae33978fb3bebb10f6411b9c8dfee8a7
Jackee-Huy-Ma <jackee.huy.ma@gmail.com>
2019-11-05 12:10:43 -0800

    Implement CreateCommandQueue function.

--- blue-tiles-engine/engine/opencl/OpenCLManager.cpp
+++ blue-tiles-engine/engine/opencl/OpenCLManager.cpp
@@ -81,3 +81,103 @@ cl_context OpenCLManager::createContext() {
 
     return context;
 }
+
+cl_command_queue OpenCLManager::CreateCommandQueue(cl_context context, cl_device_id *device) {
+    //Get number of devices
+    cl_int numDevices;
+    size_t retSize;
+    cl_int errNum = clGetContextInfo(context, CL_CONTEXT_NUM_DEVICES, sizeof(numDevices), (void *)&numDevices,
+                                     &retSize);
+    
+    //Error check on errNum.
+    if(!CheckOpenCLError(errNum, "Could not get context info!")) {
+        return NULL;
+    }
+
+    DebugLog::info("\n There are " + std::to_string(numDevices) + " devices. \n");
+
+    //Get list of devices
+    cl_device_id *deviceList;
+    deviceList = (cl_device_id *)malloc(numDevices * sizeof(cl_device_id));
+    errNum = clGetContextInfo(context, CL_CONTEXT_DEVICES, numDevices * sizeof(cl_device_id), (void *)deviceList,
+                              &retSize);
+    
+    if(!CheckOpenCLError(errNum, "Could not get device list")) {
+        DebugLog::Error(" Error code" + std::to_string(errNum));
+
+        switch (errNum) {
+            case CL_INVALID_CONTEXT:
+                DebugLog::Error(" (CL_INVALiD_CONTEXT)");
+                break;
+            case CL_INVALID_VALUE:
+                DebugLog::Error(" (CL_INVALiD_VALUE)");
+                break;
+            case CL_OUT_OF_RESOURCES:
+                DebugLog::Error(" (CL_OUT_OF_RESOURCES)");
+                break;
+            case CL_OUT_OF_HOST_MEMORY:
+                DebugLog::Error(" (CL_OUT_OF_HOST_MEMORY)");
+                break;
+            default:
+                break;
+        }
+
+        DebugLog::Error(" size = " + std::to_string(sizeof(cl_device_id)) + ";" 
+                        + std::to_string(retSize) + "\n");
+        return NULL;
+    }
+
+    // Get device information for each device
+    cl_device_type devType;
+    DebugLog::Info("\n Device List: \n");
+
+    for(int i = 0; i < numDevices; i++) {
+        DebugLog::Info(" " + std::to_string(deviceList[i]) + ":");
+        // device type
+       errNum = clGetDeviceInfo(deviceList[i], CL_DEVICE_TYPE, sizeof(cl_device_type), (void *)&devType, &retSize);
+        if (!CheckOpenCLError(errNum, "ERROR getting device info!")) {
+            free(deviceList);
+            return NULL;
+        }
+        DebugLog::Info(" type " std::toString(devType) + ":");
+
+        if (devType & CL_DEVICE_TYPE_CPU) {
+            Debug::Info(" CPU");
+        }
+
+        if (devType & CL_DEVICE_TYPE_GPU){
+            Debug::Info(" GPU");
+        }
+
+        if (devType & CL_DEVICE_TYPE_ACCELERATOR) {
+            Debug::Info(" accelerator");
+        }
+
+        if (devType & CL_DEVICE_TYPE_DEFAULT) {
+            Debug::Info(" default");
+        }
+
+        char devName[PLATFORM_NAME_SIZE];
+        errNum = clGetDeviceInfo(deviceList[i], CL_DEVICE_NAME, 1024, (void *)devName, &retSize);
+        
+        if (!CheckOpenCLError(errNum, "ERROR getting device name!")) {
+            free(deviceList);
+            return NULL;
+        }
+        DebugLog::Info(" name<" + std::to_string(devName) + "> \n");
+    }
+    
+    cl_command_queue commandQueue = clCreateCommandQueueWithProperties(context, deviceList[0], 0, NULL);
+
+    if(commandQueue == NULL) {
+        free(deviceList);
+        DebugLog::ERROR("Failed to create commandQueue for device 0");
+        return NULL;
+    }
+
+    *device = deviceList[0];
+    free(deviceList);
+    
+    return commandQueue;
+    }
+}
