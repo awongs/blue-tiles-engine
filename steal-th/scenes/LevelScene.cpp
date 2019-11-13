ac81308f27ad659ae3bbe207ed41b2c8fabfd13c
Minitte <19943415+Minitte@users.noreply.github.com>
2019-11-12 23:26:43 -0800

    Created guard prefab

--- steal-th/scenes/LevelScene.cpp
+++ steal-th/scenes/LevelScene.cpp
@@ -33,6 +33,7 @@
 
 #include "../prefabs/ObjectItemPrefab.h"
 #include "../prefabs/PlayerPrefab.h"
+#include "../prefabs/GuardPrefab.h"
 
 constexpr glm::vec3 RED = glm::vec3(1, 0, 0);
 constexpr glm::vec3 GREEN = glm::vec3(0, 1, 0);
@@ -125,98 +126,16 @@ LevelScene::LevelScene(Level* level, PhysicsEngine *physEngine)
 	// Add to world
 	AddWorldGameObject(playerObj);
 
-	// Play music
-	SoundManager::getInstance().getMusic("music")->play();
-
 	// Create the guards
 	for (Guard &guard : level->m_guards)
 	{
-		AnimatedMesh* animatedMesh = new AnimatedMesh("../Assets/models/robot_kyle.obj", "../Assets/animations/robot_kyle/KyleWalking.dae");
-		animatedMesh->SetTexture("../Assets/textures/robot_kyle.png");
+		GameObject* guardGO = Prefab::CreateGuardGameObject(m_physEngine, this, playerObj, &guard);
 
-		glm::vec3 position = glm::vec3(
-			(float)(guard.tileX) * TILE_SIZE + TILE_SIZE / 2.f,
-			0,
-			(float)(guard.tileZ) * TILE_SIZE + TILE_SIZE / 2.f);
-		std::unique_ptr<GameObject> ga = std::make_unique<GameObject>("guard",
-			position, glm::vec3(0, glm::radians(guard.rotAngle), 0), glm::vec3(5, 5, 5));
-
-		ga->AddBehaviour(animatedMesh);
-
-		// Animations for guards.
-		std::shared_ptr<Animation> walk = FileManager::LoadAnimation("../Assets/animations/robot_kyle/KyleWalking.dae");
-		std::shared_ptr<Animation> idle = FileManager::LoadAnimation("../Assets/animations/robot_kyle/KyleIdle.dae");
-		std::shared_ptr<Animation> look = FileManager::LoadAnimation("../Assets/animations/robot_kyle/KyleLooking.dae");
-
-		Animator* animator = new Animator(ga->GetBehaviour<AnimatedMesh>());
-		ga->AddBehaviour(animator);
-		animator->AddAnimation(walk);
-		animator->AddAnimation(idle);
-		animator->AddAnimation(look);
-
-		// Add physics behaviour.
-		Collider* guardCol{ new Collider(glm::vec3(1.5f)) };
-		ga->AddBehaviour(new PhysicsBehaviour(m_physEngine, ga->id, guardCol));
-
-		// Add guard detection behaviour
-		ga->AddBehaviour(new GuardDetection(this, playerObj,
-			guard.tileViewDistance * LevelScene::TILE_SIZE, guard.tileViewRadius));
-
-		SimpleGuardMovementAIBehaviour* sgmaib = new SimpleGuardMovementAIBehaviour(10.0f, glm::radians(180.0f));
-
-		/*// move to box
-		sgmaib->AddMoveTileAction(1, 2);
-		sgmaib->AddTurnCWAction();
-		sgmaib->AddMoveTileAction(1, 1);
-		sgmaib->AddTurnCWAction();
-		sgmaib->AddTurnCWAction();
-		sgmaib->AddWaitAction(2);
-
-		// move back
-		sgmaib->AddMoveTileAction(1, 2);
-		sgmaib->AddTurnCCWAction();
-		sgmaib->AddMoveTileAction(2, 2);
-		sgmaib->AddTurnCWAction();
-		sgmaib->AddTurnCWAction();
-		sgmaib->AddWaitAction(2);*/
-
-		// Setting guard movement
-		for (std::string move : guard.movement) {
-			if (move == "turncw") {
-				sgmaib->AddTurnCWAction();
-			}
-			else if (move == "turnccw") {
-				sgmaib->AddTurnCCWAction();
-			}
-			else if (move.find("move") != std::string::npos) {
-				std::string value1;
-				std::string value2;
-				bool split = false;
-				for (int i = 5; i < std::strlen(move.c_str()); i++) {
-					if (move[i] == ',') {
-						split = true;
-					}
-					if (move[i] != ',' && split == false) {
-						value1 += move[i];
-					}
-					else if(move[i] != ',' && split == true){
-						value2 += move[i];
-					}
-				}
-				//DebugLog::Info(value1 + " " + value2);
-				sgmaib->AddMoveTileAction(std::stoi(value1), std::stoi(value2));
-			}
-		}
-
-		ga->AddBehaviour(sgmaib);
-
-		// Add a spot light in front of the guard
-		float theta = atan2f(guard.tileViewRadius * LevelScene::TILE_SIZE, guard.tileViewDistance * LevelScene::TILE_SIZE);
-		SpotLight* guardCone = new SpotLight(glm::vec3(1), ga->forward, theta, theta * 1.25f);
-		ga->AddBehaviour(guardCone);
-
-		m_worldGameObjects.push_back(std::move(ga));
+		AddWorldGameObject(guardGO);
 	}
+
+	// Play music
+	SoundManager::getInstance().getMusic("music")->play();
 
 	// UI for level
 	GameObject* menu = new GameObject();
